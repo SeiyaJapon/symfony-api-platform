4c592182e1a673295763f8e676b8a7d20ed9a464
Fran <fperez@eventscase.com>
2021-02-18 15:37:37 +0100

    User repository

--- api/src/Repository/BaseRepository.php
+++ api/src/Repository/BaseRepository.php
@@ -5,8 +5,13 @@ declare(strict_types=1);
 namespace App\Repository;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DBALException;
+use Doctrine\DBAL\Exception;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Persistence\Mapping\MappingException;
 use Doctrine\Persistence\ObjectRepository;
 
 abstract class BaseRepository
@@ -29,7 +34,7 @@ abstract class BaseRepository
 
     /**
      * @param object $entity
-     * @throws \Doctrine\ORM\ORMException
+     * @throws ORMException
      */
     public function persistEntity(object $entity) : void
     {
@@ -37,9 +42,9 @@ abstract class BaseRepository
     }
 
     /**
-     * @throws \Doctrine\ORM\ORMException
-     * @throws \Doctrine\ORM\OptimisticLockException
-     * @throws \Doctrine\Persistence\Mapping\MappingException
+     * @throws ORMException
+     * @throws OptimisticLockException
+     * @throws MappingException
      */
     public function flushData() : void
     {
@@ -47,7 +52,50 @@ abstract class BaseRepository
         $this->getEntityManager()->clear();
     }
 
-    abstract protected static function entityClass: string;
+    /**
+     * @param object $entity
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function saveEntity(object $entity)
+    {
+        $this->getEntityManager()->persist($entity);
+        $this->getEntityManager()->flush();
+    }
+
+    /**
+     * @param object $entity
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function removeEntity(object $entity)
+    {
+        $this->getEntityManager()->remove($entity);
+        $this->getEntityManager()->flush();
+    }
+
+    /**
+     * @param string $query
+     * @param array  $params
+     * @return array
+     * @throws Exception
+     */
+    public function executeFetchQuery(string $query, array $params) : array
+    {
+        return $this->connection->executeQuery($query, $params)->fetchAll();
+    }
+
+    /**
+     * @param string $query
+     * @param array  $params
+     * @throws Exception
+     */
+    public function executeQuery(string $query, array $params) : void
+    {
+        $this->connection->executeQuery($query, $params);
+    }
+
+    abstract protected static function entityClass() : string;
 
     /**
      * @return ObjectRepository|EntityManager
