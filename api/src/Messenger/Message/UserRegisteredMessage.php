8a2a8f624bda5a1b48fda7d6d0102947e5f2a426
Fran <fperez@eventscase.com>
2021-02-19 13:10:11 +0100

    Install Messenger and create UserRegisteredMessage class

--- api/src/Messenger/Message/UserRegisteredMessage.php
+++ api/src/Messenger/Message/UserRegisteredMessage.php
@@ -6,5 +6,49 @@ namespace App\Messenger\Message;
 
 class UserRegisteredMessage
 {
+    private string $id;
+    private string $name;
+    private string $email;
+    private string $token;
+
+    public function __construct(string $id, string $name, string $email, string $token)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->token = $token;
+    }
+
+    /**
+     * @return string
+     */
+    public function id(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function email(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return string
+     */
+    public function token(): string
+    {
+        return $this->token;
+    }
 
 }
